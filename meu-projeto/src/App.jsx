30258a03d943ece023449ab090bbf83f47ca2445
Gyan Hugo <gyanhugo1@gmail.com>
2023-10-19 16:26:59 -0300

    finalizando

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -6,15 +6,16 @@ function App() {
   const [altura, setAltura] = useState("");
   const [peso, setPeso] = useState("");
   const [imc, setImc] = useState(null);
+  const [mostrarResultado, setMostrarResultado] = useState(false);
 
-  const calcularIMC = () => {
+  const calcularIMC = (e) => {
+    e.preventDefault();
+  
     const alturaMetros = altura / 100;
     const imcCalculado = peso / (alturaMetros * alturaMetros);
-    setImc(imcCalculado.toFixed());
-
-    setTimeout(() => {
-      setImc(null);
-    }, 3000);
+    const imcFormatado = imcCalculado.toFixed(1).substring(0, 3);
+    setImc(imcFormatado);
+    setMostrarResultado(true);
   };
 
   return (
@@ -23,29 +24,38 @@ function App() {
         <h1>Calculadora de IMC</h1>
       </header>
       <div className="conteudo">
-      <img className="imagem" src={Balanca} />
-        <form>
-        <div className="form-group">
-          <label className="label">Altura (cm):</label>
-          <input className="input"  placeholder="ex: 1.70"
-            type="number"
-            value={altura}
-            onChange={(e) => setAltura(e.target.value)}
-          />
-          <label className="label">Peso (kg):</label>
-          <input className="input" placeholder="ex: 60.5"
-            type="number"
-            value={peso}
-            onChange={(e) => setPeso(e.target.value)}
-          />
-        </div>
-        <button onClick={calcularIMC} className="click">Calcular IMC</button>
-        <button type="reset" className="reset">Limpar</button>
-        
-          <div className="resposta">
-            <h2>Seu IMC é {imc}</h2>
+        <img className="imagem" src={Balanca} alt="Balança" />
+        <form onSubmit={calcularIMC}>
+          <div className="form-group">
+            <label className="label">Altura (cm):</label>
+            <input
+              className="input"
+              placeholder="ex: 1.70"
+              type="number"
+              value={altura}
+              onChange={(e) => setAltura(e.target.value)}
+            />
+            <label className="label">Peso (kg):</label>
+            <input
+              className="input"
+              placeholder="ex: 60.5"
+              type="number"
+              value={peso}
+              onChange={(e) => setPeso(e.target.value)}
+            />
           </div>
-        
+          <button type="submit" className="click">
+            Calcular IMC
+          </button>
+          <button type="reset" className="reset">
+            Limpar
+          </button>
+
+          {mostrarResultado && (
+            <div className="resposta">
+              <h2>Seu IMC é {imc}</h2>
+            </div>
+          )}
         </form>
       </div>
     </div>
